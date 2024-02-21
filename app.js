95f043c873d9b5b17dedcfc309ac798d30276bce
Vivien-Parsis <vivienparsis.perso@gmail.com>
2024-02-21 16:15:36 +0100

    added sign in and sign up

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ fastify.post('/api/message/send', (request, reply) => {
     const email = request.body.email ? request.body.email : ""
     const message = request.body.message ? request.body.message : ""
     fs.appendFile('./public/log/message.txt',`email:${email}|message:${message}|date:${new Date()}\n`, (err)=>{
-        if(err){
+        if(err){    
             console.log(err)
         }
     })
@@ -36,6 +36,44 @@ fastify.get('/api/film/get',(request,reply)=>{
 fastify.get('/api/serie/get',(request,reply)=>{
     reply.headers({'Access-Control-Allow-Origin':'*'},{'Content-type':'application/json'})
     reply.sendFile('json/serie.json')
+})
+
+fastify.post('/api/login/signin',(request,reply)=>{
+    const email = request.body.email ? request.body.email : ""
+    const password = request.body.password ? request.body.password : ""
+    reply.headers({'Access-Control-Allow-Origin':'*'},{'Content-type':'application/json'})
+    const users = require("./user.json")
+    for(let user of users){
+        if(user.email==email && user.password==password){
+            reply.send(user);
+            return;
+        }
+    }
+    reply.send({"message":"erreur"})
+})
+fastify.post('/api/login/signup',(request, reply)=>{
+    const email = request.body.email ? request.body.email : ""
+    const password = request.body.password ? request.body.password : ""
+    if(email.trim()=="" || password.trim()==""){
+        reply.send({"message":"erreur"})
+        return
+    }
+    const users = require("./user.json")
+    for(let user of users){
+        if(user.email==email && user.password==password){
+            reply.send({"message":"user already exist"});
+            return;
+        }
+    }
+    users.push({"email":email,"password":password})
+    fs.writeFile("./user.json",JSON.stringify(users),(err)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("successfull signup")
+        }
+    })
+    reply.send({"message":"successfully sign up"})
 })
 
 fastify.listen({host: host, port: port }, (err, address) => {
